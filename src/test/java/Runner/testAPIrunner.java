d2a5b6eb6e0764117a8ec041c78d252525a4144a
alpi <alpilail17@gmail.com>
2022-04-15 11:47:17 +0700

    change tags @all

--- src/test/java/Runner/testAPIrunner.java
+++ src/test/java/Runner/testAPIrunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = {"stepdefinition"},
         plugin = {"pretty", "html:target/cucumber/report.html","json:target/cucumber/report.json"},
-        tags = "@GET",
+        tags = "@all",
         features = {"src/test/java/features"})
 
 public class testAPIrunner {
